918ddbf8aa2c48aaeaa5dd46091d4b44561f7dea
Dhave Dayao <xambuddy.ph@gmail.com>
2019-09-21 19:53:26 +0800

    Fixed issue on statuses
    Fixed issue on statuses

--- RoomVoiceController/RoomVoiceController/RoomVoiceController/ViewModels/RoomControllerViewModel.cs
+++ RoomVoiceController/RoomVoiceController/RoomVoiceController/ViewModels/RoomControllerViewModel.cs
@@ -47,14 +47,41 @@ namespace RoomVoiceController.ViewModels
 
         private void SpeechToTextFinalResultRecieved(string args)
         {
-            if(args.ToLower().Contains("main light on"))
-            {
+            if(args.ToLower().Contains("door lock"))
+                DoorLockStatus = true;
+            if (args.ToLower().Contains("door unlock"))
+                DoorLockStatus = false;
+
+            if (args.ToLower().Contains("main light on"))
                 MainLightStatus = true;
-            }
             if (args.ToLower().Contains("main light off"))
-            {
                 MainLightStatus = false;
+
+            if (args.ToLower().Contains("aircon on"))
+                AirconStatus = true;
+            if (args.ToLower().Contains("aircon off"))
+                AirconStatus = false;
+
+            if (args.ToLower().Contains("tv on"))
+                TelevisionStatus = true;
+            if (args.ToLower().Contains("tv off"))
+                TelevisionStatus = false;
+
+            if (args.ToLower().Contains("dim light on"))
+                DimLightStatus = true;
+            if (args.ToLower().Contains("dim light off"))
+                DimLightStatus = false;
+
+            if (args.ToLower().Contains("sleep mode"))
+            {
+                DoorLockStatus = true;
+                MainLightStatus = false;
+                TelevisionStatus = false;
+                AirconStatus = true;
+                DimLightStatus = true;
             }
+
+            SendData();
         }
 
         public ICommand VoiceCommand { get; set; }
@@ -63,16 +90,48 @@ namespace RoomVoiceController.ViewModels
         {
             string data = "#$";
 
+            if (DoorLockStatus)
+                data += "1";
+            else
+                data += "0";
+
             if (MainLightStatus)
                 data += "1";
             else
                 data += "0";
 
-            data += "1111";
+            if (AirconStatus)
+                data += "1";
+            else
+                data += "0";
+
+            if (TelevisionStatus)
+                data += "1";
+            else
+                data += "0";
+
+            if (DimLightStatus)
+                data += "1";
+            else
+                data += "0";
 
             data += "%";
 
             Shell.BTSocket?.WriteData(data);
+        }
+
+        private bool _DoorLockStatus;
+        public bool DoorLockStatus
+        {
+            get
+            {
+                return _DoorLockStatus;
+            }
+            set
+            {
+                _DoorLockStatus = value;
+                OnPropertyChanged(nameof(DoorLockStatus));
+            }
         }
 
         private bool _MainLightStatus;
@@ -86,7 +145,48 @@ namespace RoomVoiceController.ViewModels
             {
                 _MainLightStatus = value;
                 OnPropertyChanged(nameof(MainLightStatus));
-                SendData();
+            }
+        }
+
+        private bool _AirconStatus;
+        public bool AirconStatus
+        {
+            get
+            {
+                return _AirconStatus;
+            }
+            set
+            {
+                _AirconStatus = value;
+                OnPropertyChanged(nameof(AirconStatus));
+            }
+        }
+
+        private bool _TelevisionStatus;
+        public bool TelevisionStatus
+        {
+            get
+            {
+                return _TelevisionStatus;
+            }
+            set
+            {
+                _TelevisionStatus = value;
+                OnPropertyChanged(nameof(TelevisionStatus));
+            }
+        }
+
+        private bool _DimLightStatus;
+        public bool DimLightStatus
+        {
+            get
+            {
+                return _DimLightStatus;
+            }
+            set
+            {
+                _DimLightStatus = value;
+                OnPropertyChanged(nameof(DimLightStatus));
             }
         }
     }
